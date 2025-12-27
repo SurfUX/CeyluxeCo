bb917a9803b64b8dcad56f3b70d38206713a22a4
SurfUX <tecnetsoftwaresolution@gmail.com>
2025-12-27 08:29:49 +0530

    Contact

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,28 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    http_response_code(403);
+    exit;
+}
 
-  $receiving_email_address = 'contact@surfux.com';
+$name    = htmlspecialchars(trim($_POST['name']));
+$email   = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$phone   = htmlspecialchars(trim($_POST['phone'] ?? ''));
+$subject = htmlspecialchars(trim($_POST['subject'] ?? 'Contact Form'));
+$message = htmlspecialchars(trim($_POST['message']));
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$to = "contact@surfux.com";
+$headers = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$body = "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Phone: $phone\n\n";
+$body .= "Message:\n$message";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+if (mail($to, $subject, $body, $headers)) {
+    echo "Message sent successfully!";
+} else {
+    echo "Failed to send message. Please try again.";
+}
 ?>
